77e703056dccc843abbabeb3881b8253a12d4cea
3zzazakl <vanzekovichgr@gmail.com>
2023-11-17 03:22:10 +0200

    add

--- add.c
+++ add.c
@@ -6,20 +6,11 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	int result;
-	stack_t *top;
-
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	result = (*stack)->n + (*stack)->next->n;
-	(*stack)->next->n = result;
-
-	top = *stack;
-	*stack = (*stack)->next;
-
-	free(top);
+	(*stack)->next->n += (*stack)->n;
+	pop(stack);
 }
