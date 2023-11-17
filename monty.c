0f3ce31268e0655ae4cafa712e7774761c3538b3
3zzazakl <vanzekovichgr@gmail.com>
2023-11-17 02:00:56 +0200

    check

--- monty.c
+++ monty.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Cannot open file %s.\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((read = getline(&line, &len, file)) != -1)
@@ -128,8 +128,8 @@ int main(int argc, char *argv[])
 			handle_invalid_instruction(opcode, line_number);
 		line_number++;
 	}
-	free_stack(stack);
 	fclose(file);
 	free(line);
+	free_stack(stack);
 	return (0);
 }
