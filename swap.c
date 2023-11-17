fb21a0db044f11a4fff92837a0862c81bb2ebbf4
3zzazakl <vanzekovichgr@gmail.com>
2023-11-17 03:12:49 +0200

    unsigned

--- swap.c
+++ swap.c
@@ -10,21 +10,21 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: Error: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	top = *stack;
 	next = top->next;
 
-	top->prev = next;
 	top->next = next->next;
+	top->prev = next;
 
 	if (next->next != NULL)
 		next->next->prev = top;
 
-	next->prev = NULL;
 	next->next = top;
+	next->prev = NULL;
 
 	*stack = next;
 }
