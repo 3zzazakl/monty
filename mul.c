169844676b08c067e14ffb41ae3b740d8b9d541a
3zzazakl <vanzekovichgr@gmail.com>
2023-11-17 20:24:29 +0200

    mul

--- mul.c
+++ mul.c
@@ -15,11 +15,11 @@ void mul(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (!is_integer((*stack)->next->n) || !is_integer((*stack)->n))
+	/* if (!is_integer((*stack)->next->n) || !is_integer((*stack)->n))
 	{
 		fprintf(stderr, "L%u: multiplication require integer values\n", line_number);
 		exit(EXIT_FAILURE);
-	}
+	} */
 	result = (*stack)->n * (*stack)->next->n;
 	(*stack)->next->n = result;
 
