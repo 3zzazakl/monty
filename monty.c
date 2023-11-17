6c9f267f3856e1f2f75a09cf8625538275882694
3zzazakl <vanzekovichgr@gmail.com>
2023-11-17 02:37:13 +0200

    unsigned

--- monty.c
+++ monty.c
@@ -124,8 +124,6 @@ int main(int argc, char *argv[])
 			else
 				handle_invalid_instruction(opcode, line_number);
 		}
-		else
-			handle_invalid_instruction(opcode, line_number);
 
 		line_number++;
 	}
