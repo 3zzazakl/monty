2b1ce43d1e4f22e583686cb87dcb5c1b22bb79d0
3zzazakl <vanzekovichgr@gmail.com>
2023-11-17 21:35:14 +0200

    rotr

--- rotr.c
+++ rotr.c
@@ -8,28 +8,20 @@
 
 void rotr(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	stack_t *last, *new_top;
+	stack_t *last = *stack;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next ==  NULL)
+	{
 		return;
+	}
 
-	last = *stack;
 	while (last->next != NULL)
 	{
 		last = last->next;
 	}
-
-	new_top = last->next;
-	new_top->prev = NULL;
-	last->next = NULL;
-
-	while (new_top->next != NULL)
-	{
-		new_top = new_top->next;
-	}
-
-	new_top->next = *stack;
-	(*stack)->prev = new_top;
-	*stack = new_top->next;
-	(*stack)->prev = NULL;
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	(*stack)->prev = last;
+	*stack = last;
 }
